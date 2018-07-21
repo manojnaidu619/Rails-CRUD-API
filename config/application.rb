c400c4ff395137e9e6e26bd90904c815061bdea5
Manoj Naidu <manojnaidu619@gmail.com>
2018-07-21 15:22:32 +0530

    precompiling issues

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Railsapi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.assets.initialize_on_precompile = false
+    config.assets.precompile = []
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
